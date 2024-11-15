1148d6e455c8d41803c39fa183a4f84faa0c156a
Frank Löffler <frank.loeffler@uni-jena.de>
2024-11-15 12:44:48 +0100

    also, for an overview, generadte a pdf with all submissions

--- group_composition_plot/create_paper_plots.sh
+++ group_composition_plot/create_paper_plots.sh
@@ -5,6 +5,10 @@ submissions/2023-12-06/Scientific_Software_Center_2023-12-21_13-13-27.json \
 submissions/2023-12-06/University_of_Reading_2023-12-11_10-18-14.json \
 submissions/2023-12-06/Princeton_University_2023-11-29_18-33-21.json
 
+./group_composition_plot.py --legend \
+--outfile group_composition_plot_all.pdf \
+submissions/2023-12-06/*.json
+
 find submissions/2023-12-06 -type f -name "*.json" -print0 | \
   xargs -0 -P 0 -I{} \
   bash -c 'f=$(basename "{}"); ./group_composition_plot.py --outfile pdf/"${f%.json}.pdf" "{}"'
